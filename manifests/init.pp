07627c37867bad5fbd359fecc38654d48af584b6
Florian Weber <f.weber@digi-info.de>
2013-04-10 22:03:49 +0200

    Replace hyphen in class name with underscore.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,23 @@
-class ub-dnsmasq {
+class ub_dnsmasq {
   $ip = '192.168.33.1'
-  $executable = '/opt/boxen/repo/shared/ub-dnsmasq/files/loopback.sh'
+  $executable = '/opt/boxen/repo/shared/ub_dnsmasq/files/loopback.sh'
 
   class { 'dnsmasq::config':
-    configfile_source => 'puppet:///modules/ub-dnsmasq/dnsmasq.conf'
+    configfile_source => 'puppet:///modules/ub_dnsmasq/dnsmasq.conf'
   }
 
   class { 'dnsmasq':
     require => Class['dnsmasq::config'],
   }
 
-  file { '/Library/LaunchDaemons/dev.ub-dnsmasq.loopback.plist':
-    content => template('ub-dnsmasq/dev.ub-dnsmasq.loopback.plist.erb'),
+  file { '/Library/LaunchDaemons/dev.ub_dnsmasq.loopback.plist':
+    content => template('ub_dnsmasq/dev.ub_dnsmasq.loopback.plist.erb'),
     group   => 'wheel',
-    notify  => Service['dev.ub-dnsmasq.loopback'],
+    notify  => Service['dev.ub_dnsmasq.loopback'],
     owner   => 'root'
   }
 
-  service { 'dev.ub-dnsmasq.loopback':
+  service { 'dev.ub_dnsmasq.loopback':
     ensure  => running,
     require => Package['boxen/brews/dnsmasq']
   }
